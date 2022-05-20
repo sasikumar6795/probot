91ecd4df56279918a5e73231b65756ff3ea60760
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2022-05-20 19:00:58 +0530

    working app with static data

--- index.js
+++ index.js
@@ -2,20 +2,14 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  * 
+ * 
+ * 
  */
-
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
-
   app.on("pull_request.opened", async (context) => {
-    //app.log.info(context.payload);
     const data = context.payload;
-    // const issueComment = context.payload({
-    //   body: "Thanks for raising the pull request"
-    // });
-    // return context.octokit.issues.createComment(issueComment);
-
     // reading data from api 
     
     const cloneUrl =data.repository?.clone_url;
@@ -29,37 +23,14 @@ module.exports = (app) => {
     }
 
     let stringifiedData = JSON.stringify(mockDataSending);
-  
-    const spawn = require('child_process').exec;
-    //const pythonPath="C:\\Users\\sasik\\AppData\\Local\\Programs\\Python\\Python310"; 
 
-    const process = spawn('python', ['reviewer.py',stringifiedData]);
-
-    var result = '';
-    app.log.info("result", result);  
-    process.stdout.on('data', function (stdData) {
-      app.log.info("stdData",stdData);
-      result += stdData.toString();
+    //adding reviewers
+    const reviewers = context.pullRequest({
+      reviewers:['sasikumar6795', 'ReshmaManoharan']
     });
-    //let resultData = JSON.parse(result);
-    app.log.info("result", result);
-    const resultData='';
-    process.stderr.on('end', function () {
-  
-      // Parse the string as JSON when stdout
-      // data stream ends
-      resultData = JSON.parse(result);
-    });
-    app.log.info("resultData ", resultData);
-    // const assignee =  context.issue({
-    //   assigness: ["sasikumar6795","Arunrajg"]
-    // })
+   
+    context.octokit.rest.pulls.requestReviewers(reviewers);
 
-    // context.octokit.pull_request.addAssignees(assignee);
-
-    //   return context.octokit.issues.createComment(issueComment);;
-  // });
-
-    return context.octokit.pull_request;
+    return context.octokit.pulls.createReviewComment("done");
   
 })};
